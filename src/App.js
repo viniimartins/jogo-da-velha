c36620daf24ab08f4f5b1927ca4771ff5bb29fa3
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2023-03-24 16:36:17 -0300

    testando 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TESTANDO <code>src/App.js</code> and save to reload.
+          TESTANDO 2 <code>src/App.js</code> aloaloalo
         </p>
         <a
           className="App-link"
