bc6310bbc5ae7ec8aa81e2e6212fcafe29fb8afb
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2023-03-29 15:30:22 -0300

    Comecamos a nossa arquitetura de pastas

--- .history/src/index_20230329151434.js
+++ .history/src/index_20230329151434.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/settings/colors.css'
 import './styles/generic/reset.css'
 import './styles/elements/base.css'
 import App from './App';
--- .history/src/index_20230329152827.js
+++ .history/src/index_20230329152827.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/settings/colors.css'
 import './styles/generic/reset.css'
 import './styles/elements/base.css'
+import './styles/settings/color.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
