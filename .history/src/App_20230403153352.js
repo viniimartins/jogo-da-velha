24dcdb6814f26106026237d685a8bdc8f1802911
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2023-04-03 15:38:35 -0300

    Criamos nosso object Player

--- .history/src/App_20230403153352.js
+++ .history/src/App_20230403153352.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import Card from "./objects/Card"
+
 import Header from "./components/Header"
 import Player from "./objects/Player";
 
---
+++ .history/src/objects/Player/index_20230403153057.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Player = () => {
+    return (
+        <button className="player">
+
+        </button>
+    )
+}
+
+export default Player;
